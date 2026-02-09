9d0886e2a40a6fa7bc517405ff0e2b4fe98a8902
Shreya0777 <singhsshreya297@gmail>
2026-02-09 10:28:15 +0530

    learning the next() function

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ const express = require('express');
 const app=express();
 
 
-app.get("/user/:userid/:name/:password",(req,res)=>{
-    console.log(req.params);
-    res.send({firstname : "Shreya",lastname:"Singh"})
+app.get("/user",(req,res,next)=>{
+    console.log("1st route");
+    next();
+},
+(req,res,next)=>{
+    console.log("2nd rout");
+    next();
+},(req,res,next)=>{
+    console.log("3rd route");
+    res.send("hello from 3rd route");
+    next();// goes in thecall stack and return immediately as there is no more code to execute
 });
 
 
