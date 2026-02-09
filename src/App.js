9de15a6a463c06ca0019383fe7d1b14225342424
Shreya0777 <singhsshreya297@gmail>
2026-02-09 09:57:46 +0530

    Query,params,more about routing

--- src/App.js
+++ src/App.js
@@ -3,32 +3,10 @@ const express = require('express');
 const app=express();
 
 
-//request handler
-app.use("/test", (req,res)=>{
-    res.send("This is test route");
+app.get("/user/:userid/:name/:password",(req,res)=>{
+    console.log(req.params);
+    res.send({firstname : "Shreya",lastname:"Singh"})
 });
-
-app.get('/user',(req,res) =>{
-    res.send({firstname: "Shreya", Lastname: "Singh"});
-})
-
-app.post('/user',(req,res)=>{
-    res.send('user created successfully');
-})
-
-app.delete('/user' , (req,res) =>{
-    res.send('user deleted successfully')
-})
-
-// app.use("/hello",(req,res)=>{
-//     res.send("Hello world");
-// });
-
-
-// app.use("/",(req,res)=>{
-//     res.send("Hello from server");
-// });
-
 
 
 app.listen(3000,()=>{
